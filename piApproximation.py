839cc2fd42142852ef69aa576dbe2e3f5964ffd2
Chenhao Huang <109107564+GoldenHCH@users.noreply.github.com>
2024-05-17 10:07:20 -0600

    Update piApproximation.py

--- piApproximation.py
+++ piApproximation.py
@@ -1,10 +1,7 @@
 import numpy as mp
 
-x = []
-y = []
-for i in range (0,10000):
-  x.append(np.random.uniform(-1,1))
-  y.append(np.random.uniform(-1,1))
+x = np.random.uniform(-1, 1, size=[10000, 1])
+y = np.random.uniform(-1, 1, size=[10000,1])
 
 inside = (x**2 + y**2) < 1
 in_x = x[inside]
